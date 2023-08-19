e404baf3f6efd387a28cd2885e49374c55a7829c
JaeJinByun <byunjin11@naver.com>
2023-08-19 18:01:50 +0900

    Initial commit: 프로젝트 초기 설정 및 기본 구조 구축

---
+++ src/main/java/com/illhab/illhabServer/IllhabServerApplication.java
@@ -0,0 +1,13 @@
+package com.illhab.illhabServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllhabServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IllhabServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/illhab/illhabServer/IllhabServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.illhab.illhabServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllhabServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
