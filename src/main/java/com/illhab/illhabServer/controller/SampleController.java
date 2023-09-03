3c5d0be00d02496738aaff5fb6aff1912730e0d7
jej10e5 <jej10e5@gmail.com>
2023-09-03 15:37:16 +0900

    build test용 커밋

--- src/main/java/com/illhab/illhabServer/controller/SampleController.java
+++ src/main/java/com/illhab/illhabServer/controller/SampleController.java
@@ -1,9 +1,7 @@
 package com.illhab.illhabServer.controller;
 
-import com.illhab.illhabServer.model.Sample;
 import com.illhab.illhabServer.service.SampleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -12,8 +10,4 @@ public class SampleController {
 
     private SampleService sampleService;
 
-    @GetMapping(value = "/sample")
-    public Sample getSample() {
-        return sampleService.getSample();
-    }
 }
--- src/main/java/com/illhab/illhabServer/service/SampleService.java
+++ src/main/java/com/illhab/illhabServer/service/SampleService.java
@@ -1,31 +1,9 @@
 package com.illhab.illhabServer.service;
 
-import com.illhab.illhabServer.model.Sample;
-import java.sql.Timestamp;
-import java.util.Random;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SampleService {
 
-    private static final Logger log = LoggerFactory.getLogger(SampleService.class);
-
-    public Sample getSample() {
-        try {
-            Sample sample = new Sample();
-            sample.setSample_String("This is Sample Response.");
-            sample.setSample_Integer((new Random().nextInt()));
-            sample.setSample_Double((new Random().nextDouble()));
-            sample.setSample_Timestamp(new Timestamp(System.currentTimeMillis()));
-
-            return sample;
-        } catch (Exception e) {
-            log.error(ExceptionUtils.getStackTrace(e));
-            return new Sample();
-        }
-    }
 
 }
