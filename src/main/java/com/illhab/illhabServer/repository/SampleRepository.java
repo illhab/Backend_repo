c2c058d8d5e7dc5084aab11b46ce0fe40b765f7e
jej10e5 <jej10e5@gmail.com>
2023-09-03 15:37:16 +0900

    build test용 커밋2

--- src/main/java/com/illhab/illhabServer/repository/SampleRepository.java
+++ src/main/java/com/illhab/illhabServer/repository/SampleRepository.java
@@ -1,10 +1,8 @@
 package com.illhab.illhabServer.repository;
 
-import com.illhab.illhabServer.model.Sample;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SampleRepository extends JpaRepository<Sample, Long> {
+public interface SampleRepository {
 
 }
