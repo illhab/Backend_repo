7e27c0d47eaa1963aef369b273f5e78e0c62c944
JaeJinByun <byunjin11@naver.com>
2023-09-30 17:10:13 +0900

    jpa: add to entity sample

--- src/main/java/com/illhab/illhabServer/model/Sample.java
+++ src/main/java/com/illhab/illhabServer/model/Sample.java
@@ -1,18 +1,20 @@
 package com.illhab.illhabServer.model;
 
-import java.sql.Timestamp;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
-@ToString
+@Data
+@Table(name = "sample")
+@Entity
+@NoArgsConstructor
 public class Sample {
-
-    private String sample_String;
-    private Integer sample_Integer;
-    private Double sample_Double;
-    private Timestamp sample_Timestamp;
-
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "SAMPLE_ID")
+    private Long id;
 }
