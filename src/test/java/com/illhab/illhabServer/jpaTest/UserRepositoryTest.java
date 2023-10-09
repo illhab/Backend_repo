b77d6ba5cbc16e172f894efc28741a8ec6129e63
JaeJinByun <byunjin11@naver.com>
2023-10-09 15:45:28 +0900

    :white_check_mark: test: UserRepository 테스트

---
+++ src/test/java/com/illhab/illhabServer/jpaTest/UserRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.illhab.illhabServer.jpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.illhab.illhabServer.entity.SNS_ROLE;
+import com.illhab.illhabServer.entity.User;
+import com.illhab.illhabServer.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @Transactional
+    @DisplayName("정상 유저 회원가입 및 조회 테스트 케이스")
+    public void 유저_생성() {
+        //given
+        User insertUser = userRepository.save(
+            User.builder()
+                .email("test@test.com")
+                .name("TEST")
+                .sns_role(SNS_ROLE.GOOGLE)
+                .build());
+
+        //when
+        User saveUser = userRepository.findById(insertUser.getId())
+            .orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
+
+        //then
+        assertThat(insertUser).isEqualTo(saveUser);
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("정상 유저 회원가입 회원 삭제 테스트")
+    public void 유저_삭제() {
+        //given
+        User insertUser = userRepository.save(
+            User.builder()
+                .email("test@test.com")
+                .name("TEST")
+                .sns_role(SNS_ROLE.GOOGLE)
+                .build());
+
+        //when
+        userRepository.deleteById(insertUser.getId());
+
+        //then (예외가 발생해야 성공)
+        assertThrows(IllegalArgumentException.class, () -> {
+            userRepository.findById(insertUser.getId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
+        });
+    }
+
+}
