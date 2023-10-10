0ed15ad4b1fce35ec6e43e93246d51a562cc7eaf
wlswo <byunjin11@naver.com>
2023-10-10 12:40:31 +0900

    Code 정리

--- src/main/java/com/illhab/illhabServer/controller/UserController.java
+++ src/main/java/com/illhab/illhabServer/controller/UserController.java
@@ -16,19 +16,6 @@ public class UserController {
 
     @PostMapping("/join")
     public ResponseEntity<UserDto.Response> join(@RequestBody UserDto.Request userDto) {
-//        String name = joinRequest.getName();
-//        String email = joinRequest.getEmail();
-//        String sns_role = joinRequest.getSns_role();
-//
-//        String result = userService.join(name, email, sns_role);
-//        if(result.equalsIgnoreCase("success")){
-//            return "success";
-//        } else if(result.equalsIgnoreCase("already join"))
-//        {
-//            return "already join";
-//        }else {
-//            return "fail";
-//        }
         return ResponseEntity.ok().body(userService.join(userDto));
 
     }
