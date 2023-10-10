acacc305d5de41884da1c4f48c68b323582e043e
wlswo <byunjin11@naver.com>
2023-10-10 11:01:49 +0900

    code refactoring

---
+++ src/main/java/com/illhab/illhabServer/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.illhab.illhabServer.dto;
+
+import lombok.Data;
+
+@Data
+public class JoinDto {
+
+    private String name;
+    private String email;
+    private String sns_role;
+
+}
