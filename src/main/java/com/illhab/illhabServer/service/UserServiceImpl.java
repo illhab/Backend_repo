c04e67c291de5701604c4617127a972f5cdca45b
jej10e5 <jej10e5@gamail.com>
2023-10-26 23:36:43 +0900

    중복 코드 삭제 및 코드 정리

--- src/main/java/com/illhab/illhabServer/service/UserServiceImpl.java
+++ src/main/java/com/illhab/illhabServer/service/UserServiceImpl.java
@@ -1,13 +1,13 @@
 package com.illhab.illhabServer.service;
 
 import com.illhab.illhabServer.dto.UserDto;
-import com.illhab.illhabServer.dto.UserDto.JoinResponse;
-import com.illhab.illhabServer.dto.UserDto.ListResponse;
+import com.illhab.illhabServer.dto.UserDto.*;
 import com.illhab.illhabServer.entity.User;
 import com.illhab.illhabServer.repository.UserRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +16,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDto.JoinResponse join(UserDto.JoinRequest userDto) {
+    public JoinResponse join(UserDto.JoinRequest userDto) {
 
         //가입 여부 확인
         if (userRepository.existsByEmail(userDto.getEmail())) {
@@ -40,46 +40,32 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDto.UserResponse getUser(Long userId) {
-        //해당 회원 있는지 조회
-        if (!userRepository.existsById(userId)) {
-            throw new IllegalArgumentException(); //보통 Custom Exception 생성후 처리함
-        }
-
+    public UserResponse getUser(Long userId) {
         User user = userRepository.findById(userId)
             .orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
-        return new UserDto.UserResponse(user);
+        return new UserResponse(user);
     }
 
     @Override
-    public UserDto.UpdateResponse update(Long userId, UserDto.UpdateRequest userDto) {
-        //해당 회원 있는지 조회
-        if (!userRepository.existsById(userId)) {
-            throw new IllegalArgumentException(); //보통 Custom Exception 생성후 처리함
-        }
-
+    public UpdateResponse update(Long userId, UserDto.UpdateRequest userDto) {
         User user = userRepository.findById(userId)
             .orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
 
         user.changeName(userDto.getName());
 
         //이미 존재하는 PK에 대해서 save를 사용하면 update를 진행합니다.
-        return new UserDto.UpdateResponse(userRepository.save(user));
+        return new UpdateResponse(userRepository.save(user));
 
     }
 
     @Override
-    public UserDto.DeleteResponse delete(Long userId) {
-        //해당 회원 있는지 조회
-        if (!userRepository.existsById(userId)) {
-            //없다면 false반환
-            return new UserDto.DeleteResponse(userRepository.existsById(userId));
-        }
+    public DeleteResponse delete(Long userId) {
+
         User user = userRepository.findById(userId)
             .orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
 
         user.delete();
 
-        return new UserDto.DeleteResponse(true);
+        return new DeleteResponse(true);
     }
 }
